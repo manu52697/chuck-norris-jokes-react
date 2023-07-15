9bb5a8d203b02c6faf0ec8943c0566fa421c8abd
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2023-07-15 03:32:24 +0200

    Initial repo setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { JokeContainer } from './components/containers/JokeContainer'
+
+function App() {
+
+  return (
+    <JokeContainer></JokeContainer>
+  )
+}
+
+export default App
